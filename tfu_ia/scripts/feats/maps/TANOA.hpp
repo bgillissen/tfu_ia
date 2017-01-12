2045b5543dfb802d5ae4e7c784d14d0666a2b66a
bgillissen <benjamin.gillissen@gmail.com>
2017-01-12 19:29:33 +0100

    tanoa map updated

--- tfu_ia/scripts/feats/maps/TANOA.hpp
+++ tfu_ia/scripts/feats/maps/TANOA.hpp
@@ -7,102 +7,97 @@ Description:
 		Tanoa map define
 */
 
-#define MAP_PREFIX "T"
-#define MAP_FOLDER "Tanoa"
-//#define MAP_KEYWORDS ["jungle", "water", "apex"], for later, kind of assets selection helper
-#define TOT_CURATOR 4
+MAP_PREFIX = "T";
+MAP_FOLDER = "Tanoa";
+MAP_KEYWORDS = ["jungle", "water", "apex"];
+TOT_CURATOR = 4;
+TO_HC = 3;
 
 
 //------------------------------------------------------------ AO
 
-#define AO_zones [\
-					["bala",3],\
-					["main",3],\
-					["ravi-ta",1],\
-					["saintgeorge",3],\
-					["sosovu",1],\
-					["tuvanaka",2]\
-				 ]
+AO_zones = [["bala",3],
+			["main",3],
+			["ravi-ta",1],
+			["saintgeorge",3],
+			["sosovu",1],
+			["tuvanaka",2]];
 
-#define AO_markers [\
-					["Bala Airstrip",\		//bala
-					 "Leqa",\
-					 "Cerebu",\
-					 "Muaceba",\
-					 "Balavu",\
-					 "Rautake",\
-					 "Laikoro",\
-					 "Namuvaka",\
-					 "Ferry",\
-					 "Katkoula",\
-					 "Lailai",\
-					 "Koumac",\
-					 "Comm Tower",\
-					 "Yanukka"],\
-					["Georgetown",\			//main
-					 "Regina",\
-					 "Tanouka",\
-					 "Kotomo",\
-					 "Oumere",\
-					 "Luganville",\
-					 "Vatukoulo",\
-					 "Ba",\
-					 "Blue Pearl industrial port",\
-					 "Ovau",\
-					 "La Rochelle Aerodrome",\
-					 "La Rochelle",\
-					 "Saint-Paul",\
-					 "Nicolet",\
-					 "Oua-Oue",\
-					 "Temple ruins",\
-					 "Red Spring surface mine",\
-					 "Comms Bravo",\
-					 "Mont Tanoa",\
-					 "Nasua",\
-					 "Lumberyard",\
-					 "Buawa",\
-					 "Galili",\
-					 "Jungle",\
-					 "Lobaka",\
-					 "Lakatoro",\
-					 "Cocoa plantations",\
-					 "Tanoa Airfield",\
-					 "Railway Depot",\
-					 "Diesel power plant"],\
-					["Ravi-ta"],\					//ravi-ta
-					["Harcourt",\ 					//saintgeorge
-					 "Losi",\
-					 "Moddergat",\
-					 "La Foa",\
-					 "Tobakoro",\
-					 "Blerick",\
-					 "Lijnhaven",\
-					 "Taga",\
-					 "Rereki",\
-					 "Bua Bua",\
-					 "Fortress ruins",\
-					 "Port-Boise",\
-					 "forest clearing",\
-					 "Saioko",\
-					 "Doodstil",\
-					 "Ile Saint-George",\
-					 "Saint-George Airstrip"],\
-					["Sosovu"],\					//sosovu
-					["Belfort",\ 					//tuvanaka
-					 "Tuvanaka"]\
-                   ]
+AO_markers = [["Bala Airstrip",		//bala
+               "Leqa",
+               "Cerebu",
+               "Muaceba",
+               "Balavu",
+               "Rautake",
+               "Laikoro",
+               "Namuvaka",
+               "Ferry",
+               "Katkoula",
+               "Lailai",
+               "Koumac",
+               "Comm Tower",
+               "Yanukka"],
+              ["Georgetown",			//main
+               "Regina",
+               "Tanouka",
+               "Kotomo",
+               "Oumere",
+               "Luganville",
+               "Vatukoulo",
+               "Ba",
+               "Blue Pearl industrial port",
+               "Ovau",
+               "La Rochelle Aerodrome",
+               "La Rochelle",
+               "Saint-Paul",
+               "Nicolet",
+               "Oua-Oue",
+               "Temple ruins",
+               "Red Spring surface mine",
+               "Comms Bravo",
+               "Mont Tanoa",
+               "Nasua",
+               "Lumberyard",
+               "Buawa",
+               "Galili",
+               "Jungle",
+               "Lobaka",
+               "Lakatoro",
+               "Cocoa plantations",
+               "Tanoa Airfield",
+               "Railway Depot",
+               "Diesel power plant"],
+              ["Ravi-ta"],					//ravi-ta
+              ["Harcourt", 					//saintgeorge
+               "Losi",
+               "Moddergat",
+               "La Foa",
+               "Tobakoro",
+               "Blerick",
+               "Lijnhaven",
+               "Taga",
+               "Rereki",
+               "Bua Bua",
+               "Fortress ruins",
+               "Port-Boise",
+               "forest clearing",
+               "Saioko",
+               "Doodstil",
+               "Ile Saint-George",
+               "Saint-George Airstrip"],
+              ["Sosovu"],					//sosovu
+              ["Belfort", 					//tuvanaka
+               "Tuvanaka"]];
 
 //------------------------------------------------------------ FOB
 
-#define FOBS [\
-                     ["","","","","","","","",""],\	//bala
-                   	 ["","","","","","","","",""],\	//main
-                   	 [],\							//ravi-ta
-                   	 ["","","","","","","","",""],\	//saintgeorge
-                   	 [],\							//sosovu
-                   	 []\							//tuvanaka
-                    ]
+FOBS = [["","","","","","","","",""],	//bala
+        ["","","","","","","","",""],	//main
+        [],								//ravi-ta
+        ["","","","","","","","",""],	//saintgeorge
+        [],								//sosovu
+        []];							//tuvanaka
 
 //------------------------------------------------------------ BASE
 
-#define BASES [["tuvanaka", "Tuvanaka Military Airfield", "400", "200"]]
+BASES = [["tuvanaka", "Tuvanaka Military Airfield", "400", "200"]];
