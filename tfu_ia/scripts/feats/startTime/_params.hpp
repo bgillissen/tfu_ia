2bac2ea60a78687e869757702e33b619e68866d5
bgillissen <benjamin.gillissen@gmail.com>
2017-02-08 19:08:09 +0100

    +real world date in-game

--- tfu_ia/scripts/feats/startTime/_params.hpp
+++ tfu_ia/scripts/feats/startTime/_params.hpp
@@ -1,6 +1,13 @@
-class startTime {
-	title = "Randomly set the time at mission start";
+class realDate {
+	title = "Use real world date as in-game date";
 	values[] = {0,1};
 	texts[] = {"Disabled","Enabled"};
 	default = 1;
+};
+
+class randomTime {
+	title = "Randomize start time";
+	values[] = {0,1};
+	texts[] = {"Disabled","Enabled"};
+	default = 0;
 };
